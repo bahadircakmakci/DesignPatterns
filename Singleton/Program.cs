75ae0198f8952454b97e5b51cbc2ff036678ef2b
bahadir <bahadircakmakci53@users.noreply.github.com>
2020-11-21 10:42:52 +0300

    Sigleton Adapter

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam_1 = SingletonClass.Sinif.Toplam(1, 2);
+            Console.WriteLine(toplam_1);
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingletonClass
+    {
+ 
+        private SingletonClass() { }
+
+        private static SingletonClass _tekNesne = new SingletonClass();
+     
+        public static SingletonClass Sinif { get { return _tekNesne; } }
+
+      
+        public int Toplam(int s1, int s2)
+        {
+            return s1 + s2;
+        }
+    }
+}
